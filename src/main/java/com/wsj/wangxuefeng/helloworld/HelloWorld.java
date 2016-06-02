495b889203252965e8c886d6efe0c169756d3e98
wxf <591392966@qq.com>
2016-06-02 15:32:38 +0800

    测试Maven hello world

---
+++ src/main/java/com/wsj/wangxuefeng/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+package com.wsj.wangxuefeng.helloworld;
+
+public class HelloWorld {
+	public  void sayHello() {
+		
+		System.out.println("Test Maven");
+	}
+
+	public static void main(String[] args) {
+
+		new HelloWorld().sayHello();
+	} 
+}
